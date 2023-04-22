54ead1754d130218a1fdf27dd3668f769a4efe90
jessejames2736@gmail.com <jessejames2736@gmail.com>
2023-04-22 15:00:48 -0400

    Set up a drivingAge variable as an int, aasked the question 'how old are you? To the user then converted that string response from the user into an int using the Convert.ToInt32 and compare the int that is now the usersAge and the drivingAge and print a response to the console depending on that result.

--- Conitionals-Lab-CSharp/Program.cs
+++ Conitionals-Lab-CSharp/Program.cs
@@ -3,7 +3,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, test!");
+        //Console.WriteLine("Hello, test!");
+
+        int drivingAge = 16;
+        Console.WriteLine("How old are you?");
+        int usersAge = Convert.ToInt32(Console.ReadLine());
+
+        if (usersAge >= drivingAge)
+        {
+            Console.WriteLine("You are legally able to drive.");
+        }
+        else
+        {
+            Console.WriteLine("You are not old enough to drive yet.");
+        }
     }
 }
 
